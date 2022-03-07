af2fda725b5ea13ac23ae4a4c369d44e8fb54350
Alex777Russia <shevalex777@mail.ru>
2022-03-07 21:58:51 +0300

    Completion of the class adjustment process. Added methods: timeOfDistance and requiredAmountOfFuel

--- src/classes/Vehicle.java
+++ src/classes/Vehicle.java
@@ -1,5 +1,17 @@
 package classes;
+import java.util.Scanner;
 
 public class Vehicle {
+    String type;
+    double averageSpeed;
+    double consumptionPer100Km;
 
+    public static void timeOfDistance(double distance, double averageSpeed) {
+        System.out.println(distance / averageSpeed);
+    }
+
+    public static void requiredAmountOfFuel(double distance, double consumptionPer100Km) {
+        System.out.println(distance / consumptionPer100Km * 100);
+    }
 }
+
