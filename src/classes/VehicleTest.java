22a057175fb279b5c943e2a49bd79fdb6087c427
eldor-galiev <eldor.galiev888@gmail.com>
2022-03-07 23:06:26 +0300

    first commit

---
+++ src/classes/VehicleTest.java
@@ -0,0 +1,18 @@
+package classes;
+
+import com.sun.jdi.connect.Transport;
+
+public class VehicleTest {
+    public static void main(String[] args) {
+        Vehicle vehicle = new Vehicle("BMW", 120, 8);
+
+
+        VehicleInfo(vehicle);
+    }
+
+    public static void VehicleInfo(Vehicle vehicle){
+        System.out.println("time = " + vehicle.getTime(480.0));
+        System.out.println("fuel = " + vehicle.getFuel(360.0));
+
+    }
+}
