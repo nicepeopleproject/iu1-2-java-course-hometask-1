99df8f8e0bccb1cfdf68517af115d1b17a80e981
MadMaxxx03 <babenko.md@ya.ru>
2022-03-10 22:10:36 +0300

    homework_first_commit

--- src/classes/Vehicle.java
+++ src/classes/Vehicle.java
@@ -1,5 +1,40 @@
 package classes;
+import java.util.Scanner;
 
 public class Vehicle {
+    String name;
+    int av_speed;
+    int con;
+    public static void main(String[] args) {
+        Vehicle Audi = car_factory();
+        d_time(Audi);
+        f_count(Audi);
+    }
 
+    static void d_time(Vehicle Audi){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter distance for time");
+        int distance = sc.nextInt();
+        System.out.println("Amount of time: "+distance / Audi.av_speed);
+    }
+
+    static void f_count(Vehicle Audi){
+        Scanner cs = new Scanner(System.in);
+        System.out.println("Enter distance for fuel");
+        int distance = cs.nextInt();
+        System.out.println("Amount of fuel: "+distance * Audi.con / 100);
+    }
+
+    static Vehicle car_factory(){
+        Vehicle car = new Vehicle();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Vehicle name");
+        car.name = sc.nextLine();
+        System.out.println("Enter Vehicle average speed");
+        car.av_speed = sc.nextInt();
+        System.out.println("Enter Vehicle consumption");
+        car.con = sc.nextInt();
+        //System.out.println(car.name+car.av_speed+car.con);
+        return car;
+    }
 }
--- src/functions/Function1.java
+++ src/functions/Function1.java
@@ -1,5 +1,7 @@
 package functions;
 
+import java.util.logging.SimpleFormatter;
+
 public class Function1 {
     public static void main(String[] args) {
         System.out.println("Function testing:");
@@ -7,17 +9,30 @@ public class Function1 {
                 .compareResults(findMax(new int[]{3, 4, 10, -1}), 10) ? "passed" : "failed");
 
         try {
-            findMax(new int[0]);
+            System.out.println(findMax(new int[0]));
             System.out.println("Test 2: failed.");
         } catch (RuntimeException e) {
             System.out.println("Test 2: passed.");
         }
     }
 
+
     // реализовать функцию, которая будет находить максимум в массиве
     // обработать случай, когда длина массива равна нулю
     // в этом случае выбрасывать исключение
     private static int findMax(int[] arr) {
-        return 1;
+        if (arr.length != 0){
+            int max = arr[0];
+            for (int i = 0; i < arr.length; i ++){
+                if (arr[i] > max){
+                    max = arr[i];
+                }
+            }
+            //System.out.println("Max number: "+max);
+            return max;
+        }
+        else {
+            throw new RuntimeException("Error, length = 0");
+        }
     }
 }
