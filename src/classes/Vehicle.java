184cfe6bc396db17dc1f997619e41413d531f0c8
MityokYa <cmpro5.0@gmail.com>
2022-03-11 08:50:41 +0300

    Cherkasov

--- src/classes/Vehicle.java
+++ src/classes/Vehicle.java
@@ -1,5 +1,47 @@
 package classes;
 
+import java.util.Scanner;
+
 public class Vehicle {
+    String name;
+    Float aver_speed;
+    Float fuel_con;
+
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        Vehicle myCar = newCar();
+        System.out.print("Введите дистанцию ");
+        float distance = sc.nextFloat();
+        while (distance != 0.0){
+            Time(myCar, distance);
+            FuelNeed(myCar, distance);
+            System.out.println("");
+            System.out.print("Введите дистанцию ");
+            distance = sc.nextFloat();
+        }
+    }
+
+    static Vehicle newCar(){
+        Scanner sc= new Scanner(System.in);
+        Vehicle Car = new Vehicle();
+        System.out.println("Введите название новой машины");
+        Car.name = sc.nextLine();
+        System.out.println("Введите среднюю скорость новой машины");
+        Car.aver_speed = sc.nextFloat();
+        System.out.println("Введите среднюю скорость новой машины");
+        Car.fuel_con = sc.nextFloat();
+        System.out.println("Ваша машина: "+Car.name + " " + Car.aver_speed + " " + Car.fuel_con);
+        return Car;
+    }
+
+    static void Time(Vehicle car, float d){
+        float time = d/car.aver_speed;
+        System.out.println("Время прохождения = "+time);
+    }
+
+    static void FuelNeed(Vehicle car, float d){
+        float fuel = car.fuel_con*d/100;
+        System.out.println("Необходимое кол-во баков = "+fuel);
+    }
 
 }
--- src/functions/Function2.java
+++ src/functions/Function2.java
@@ -1,5 +1,8 @@
 package functions;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 public class Function2 {
     public static void main(String[] args) {
         int[] a = new int[]{1, 2, 3, 4, 5};
@@ -20,6 +23,28 @@ public class Function2 {
     которая объединит их и вернет упорядоченный массив с порядком таким же как и a
      */
     private static int[] merge(int[] a, int[] b) {
-        return new int[]{1};
+        int[] c = new int[a.length + b.length];
+        for (int i = 0; i < a.length; i++){
+            c[i] = a[i];
+        }
+        for (int f = 0; f < b.length; f++){
+            c[f+a.length] = b[f];
+        }
+        if (c.length != 0) {
+            if (c[0] < c[1]) {
+                Arrays.sort(c);
+            } else if (c[0] > c[1]) {
+                Integer[] C = new Integer[c.length];
+                for (int i = 0; i < c.length; i++) {
+                    C[i] = c[i];
+                }
+                Arrays.sort(C);
+                Arrays.sort(C, Collections.reverseOrder());
+                for (int i = 0; i < c.length; i++) {
+                    c[i] = C[i];
+                }
+            }
+        }
+        return c;
     }
 }
