964e6a1be02281bc690392a294ad21c68488efd1
arsen-petrosov <arspetrosov@gmail.com>
2022-03-13 16:32:11 +0300

    second commit

--- src/classes/Vehicle.java
+++ src/classes/Vehicle.java
@@ -1,5 +1,34 @@
 package classes;
 
 public class Vehicle {
+    private String type;
+    private double avgSpeed;
+    private double fuelConsumption;
 
+    public Vehicle(String type, double avgSpeed, double fuelConsumption) {
+        this.type = type;
+        this.avgSpeed = avgSpeed;
+        this.fuelConsumption = fuelConsumption;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public double getAvgSpeed() {
+        return avgSpeed;
+    }
+
+    public double getFuelConsumption() {
+        return fuelConsumption;
+    }
+
+    public double distanceTime(double distance) {
+        return distance / avgSpeed;
+    }
+
+    public double fuelCalculation(double distance) {
+        return 100 / fuelConsumption * distance;
+    }
 }
+
--- src/classes/test.java
+++ src/classes/test.java
@@ -1,2 +1,22 @@
-package classes;public class test {
+package classes;
+
+public class test {
+    public static void main(String[] args) {
+        Vehicle transport_1 = new Vehicle("BMW", 130, 13);
+        Vehicle transport_2 = new Vehicle("Mercedes", 145, 11);
+        Vehicle transport_3 = new Vehicle("Audi", 150, 15);
+
+        transportInfo(transport_1);
+        transportInfo(transport_2);
+        transportInfo(transport_3);
+    }
+
+    public static void transportInfo(Vehicle transport) {
+        System.out.println("Time = " + transport.distanceTime(2000));
+        System.out.println("Fuel = " + transport.fuelCalculation(2000));
+
+        System.out.println("Type of vehicle: " + transport.getType());
+        System.out.println("Average speed of vehicle: " + transport.getAvgSpeed());
+        System.out.println("Consumption per 100 Km: " + transport.getFuelConsumption());
+    }
 }
